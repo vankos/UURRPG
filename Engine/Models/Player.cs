5a6e46b260d15664dfd8e4363e96bb21196cc7ee
Vanko <vano111ru@yandex.ru>
2021-04-30 21:23:27 +0300

    add  game session class and engine project files

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints{ get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public int Credits { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,22 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player()
+            {
+                Name = "JohnDoe"
+            };
+        }
+    }
+}
