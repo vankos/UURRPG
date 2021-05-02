fc7a10f48c0a15bd4ed544e40cd7849ded3fbc3a
Vanko <vano111ru@yandex.ru>
2021-05-02 13:18:50 +0300

    inherit Player from INotifyPropertyChanged

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,12 +1,76 @@
-﻿namespace Engine.Models
+﻿using System.ComponentModel;
+
+namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string CharacterClass { get; set; }
-        public int HitPoints{ get; set; }
-        public int Experience { get; set; }
-        public int Level { get; set; }
-        public int Credits { get; set; }
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _experience;
+        private int _level;
+        private int _credits;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public string CharacterClass
+        {
+            get { return _characterClass; }
+            set
+            {
+                _characterClass = value;
+                OnPropertyChanged(nameof(CharacterClass));
+            }
+        }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+        public int Experience
+        {
+            get { return _experience; }
+            set
+            {
+                _experience = value;
+                OnPropertyChanged(nameof(Experience));
+            }
+        }
+        public int Level
+        {
+            get { return _level; }
+            set
+            {
+                _level = value;
+                OnPropertyChanged(nameof(Level));
+            }
+        }
+        public int Credits
+        {
+            get { return _credits; }
+            set
+            {
+                _credits = value;
+                OnPropertyChanged(nameof(Credits));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
