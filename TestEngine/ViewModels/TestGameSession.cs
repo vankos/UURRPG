5c8663ea43a2e639e6009cd726bbe6cbc572df88
Vanko <vano111ru@yandex.ru>
2021-06-01 19:12:44 +0300

    add unit test project

---
+++ TestEngine/ViewModels/TestGameSession.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine.ViewModels;
+
+namespace TestEngine.ViewModels
+{
+    [TestClass]
+    public class TestGameSession
+    {
+        [TestMethod]
+        public void  TestCreateGameSession()
+        {
+            GameSession gameSession = new GameSession();
+            gameSession.StartTheGame();
+
+            Assert.IsNotNull(gameSession.CurrentPlayer);
+            Assert.AreEqual("Basement Realm", gameSession.CurrentLocation.Name);
+        }
+
+        [TestMethod]
+        public void TestPlayerMovesToStartAndIsHealedOnKilled()
+        {
+            GameSession gameSession = new GameSession();
+            gameSession.StartTheGame();
+            gameSession.CurrentPlayer.TakeDamage(999);
+
+            Assert.AreEqual("Basement Realm", gameSession.CurrentLocation.Name);
+            Assert.AreEqual(gameSession.CurrentPlayer.Health, gameSession.CurrentPlayer.MaxHealth);
+        }
+    }
+}
